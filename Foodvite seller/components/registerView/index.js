03c535b253d461199522f144e878060031f693b5
Santhosh Narayanan <santhosh.nus@gmail.com>
2016-02-27 07:14:10 +0000

    Changes made by Santhosh Narayanan (santhosh.nus@gmail.com).

--- Foodvite seller/components/registerView/index.js
+++ Foodvite seller/components/registerView/index.js
@@ -1,29 +1,94 @@
 'use strict';
 
 app.registerView = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+    init: function () {},
+    onShow: function () {
+        $('#error-message-1').hide();
+        $('#error-message-2').hide();
+    },
+    afterShow: function () {
+        var validator = $('#registerForm').kendoValidator({
+            validateOnBlur: false
+        }).data('kendoValidator');
+        var formFields = $('#registerForm').find('input');
+        formFields.on('keyup keypress blur change input', function () {
+            validator.hideMessages();
+            if (validator.validate()) {
+                $('#register-submit').removeClass('disabled');
+            } else {
+                $('#register-submit').addClass('disabled');
+            }
+        });
+    }
+
 });
 
 // START_CUSTOM_CODE_registerView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
-// END_CUSTOM_CODE_registerView
-(function(parent) {
-    var registerViewModel = kendo.observable({
-        fields: {
-            sellerEmail2: '',
-            sellerContact: '',
-            sellerContactPerson: '',
-            sellerPincode: '',
-            sellerAddress: '',
-            sellerName: ''
-        },
-        submit: function() {app.mobileApp.navigate('components/registersuccessView/view.html');}
-    });
 
+// END_CUSTOM_CODE_registerView
+(function (parent) {
+    var registerViewModel = kendo.observable({
+            fields: {
+                sellerEmail2: '',
+                sellerContact: '',
+                sellerContactPerson: '',
+                sellerPincode: '',
+                sellerAddress: '',
+                sellerName: ''
+            },
+            submit: function () {
+                var os = kendo.support.mobileOS;
+                $.ajax({
+                    url: "http://sit.foodvite.co:81/FVS/FV",
+                    type: "POST",
+                    contentType: "application/x-www-form-urlencoded",
+                    data: {
+                        data: JSON.stringify({
+                            "serviceName": "SignUp",
+                            "name": $("#sellerName").val(),
+                            "address": $("#sellerAddress").val(),
+                            "pincode": $("#sellerPincode").val(),
+                            "contact": $("#sellerContact").val(),
+                            "contactperson": $("#sellerContactPerson").val(),
+                            "email": $("#sellerEmail").val(),
+                            "deviceType": os.device,
+                            "deviceID": os.device.uid
+                        })
+                    },
+                    dataType: "json",
+                    success: function (data) {
+                        var ob = JSON.parse(JSON.stringify(data));
+                        var errorCode = ob.resultCode;
+                        var message = ob.msg;
+                        if (errorCode == "101") {
+                            $('#registerForm')[0].reset();
+                            app.mobileApp.navigate('components/registersuccessView/view.html');
+                        }
+                        if (errorCode == "100") {
+                            alert(message);
+
+                        }
+
+                    },
+                    error: function () {
+                        $('#error-message-1').show();
+                        $('#error-message-1').fadeOut(3000);
+
+                        ;
+                    }
+                });
+            }
+        }
+
+    );
     parent.set('registerViewModel', registerViewModel);
 })(app.registerView);
+
+
+
+
 
 // START_CUSTOM_CODE_registerViewModel
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
