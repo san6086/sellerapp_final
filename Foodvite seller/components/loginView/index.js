b91831804d1a0c0cc56578ec282e12650d3c74a7
Santhosh Narayanan <santhosh.nus@gmail.com>
2016-04-03 14:19:35 +0000

    Changes made by Santhosh Narayanan (santhosh.nus@gmail.com).

--- Foodvite seller/components/loginView/index.js
+++ Foodvite seller/components/loginView/index.js
@@ -28,13 +28,77 @@ app.loginView = kendo.observable({
 (function (parent) {
     var loginViewModel = kendo.observable({
         fields: {
-            sellerPassword: '',
-            sellerEmail: '',
+
+            sellerloginEmail: '',
+            sellerloginPassword: '',
+            sellerchangepasswordEmail:''
         },
-        submit: function () {      
-               app.mobileApp.navigate('components/ordersView/view.html');
-        },
-        cancel: function () {}
+        submit: function () {
+            var os = kendo.support.mobileOS;
+            $.ajax({
+                url: "http://sit.foodvite.co:8080/FVS/v1/signin",
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    "emailId": $("#sellerloginEmail").val(),
+                    "password": $("#sellerloginPassword").val(),
+                    "deviceID": "deviceID",
+                    "deviceType": os.device
+                }),
+                dataType: "json",
+                success: function (data) {
+                    
+                    var ob = JSON.parse(JSON.stringify(data));
+                    var dataSource = new kendo.data.DataSource({ data: ob});
+                    var errorCode = ob.responseCode;
+                    var responsemessage = ob.message;
+                    var apikey = ob.data["apiToken"];
+                    var sellerId = ob.data["sellerId"];
+                    var emailId = $("#sellerloginEmail").val(); 
+                    localStorage.setItem('email',emailId);
+                    localStorage.setItem('apikey',apikey);
+                    localStorage.setItem('sellerId',sellerId);
+                    if (errorCode == 101) {
+                        $('#loginForm')[0].reset();
+                        app.mobileApp.navigate('components/ordersView/view.html');
+                    } 
+                    else if (errorCode == 100) {
+                        $('#loginForm')[0].reset();
+                        alert(responsemessage);
+                        
+                    }
+                },
+                error: function () {
+                    $('#error-message-1').show();
+                    $('#error-message-1').fadeOut(3000);
+                   
+                }
+            }); },
+        passwordsubmit: function () {
+            $.ajax({
+                url: "http://sit.foodvite.co:8080/FVS/v1/forgetPassword",
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    "email": $("#sellerpasswordEmail").val()                  
+                }),
+                dataType: "json",
+                success: function (data) {
+                   var ob1 = JSON.parse(JSON.stringify(data));
+                    var errorCode1 = ob1.responseCode;
+                    var responsemessage1 = ob1.message;                
+                    if (errorCode1 == 101) {
+                        alert(responsemessage1);
+                    } 
+                    else if (errorCode1 == 100) {
+                        
+                        alert(responsemessage1); }
+                },
+                error: function (response) {
+                 alert(response);
+                }
+            });           
+        }
     });
     parent.set('loginViewModel', loginViewModel);
 })(app.loginView);
